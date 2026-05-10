680d063b6784b5435a45fc844a336917f28547a5
Kina <gaireke@gmail.com>
2026-05-11 01:35:47 +0800

    feat: add baseline PWA support for install/offline

---
+++ service-worker.js
@@ -0,0 +1,42 @@
+const CACHE_VERSION = 'klaws-pwa-v1';
+const CORE_ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './manifest.webmanifest',
+  './icons/klaws-icon.svg',
+  './utility.js',
+  './storage.js',
+  './data.js',
+  './init.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_VERSION).then((cache) => cache.addAll(CORE_ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_VERSION).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request)
+        .then((response) => {
+          const copy = response.clone();
+          caches.open(CACHE_VERSION).then((cache) => cache.put(event.request, copy));
+          return response;
+        })
+        .catch(() => caches.match('./index.html'));
+    })
+  );
+});
