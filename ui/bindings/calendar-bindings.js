e9e56e0ac2cc9b7df65ac4964e9ccdf00f35aa8d
Kina <gaireke@gmail.com>
2026-05-12 10:42:27 +0800

    Fix calendar month switch when global cursor is undefined

--- ui/bindings/calendar-bindings.js
+++ ui/bindings/calendar-bindings.js
@@ -3,8 +3,20 @@
     const onRef=deps.on||global.on;
     const toggleCalendarView=deps.toggleCalendarView||global.toggleCalendarView;
     onRef('calendarBackBtn','click',()=>toggleCalendarView(false));
-    onRef('calendarPrevBtn','click',()=>{global.calendarCursor=new Date(global.calendarCursor.getFullYear(),global.calendarCursor.getMonth()-1,1);global.renderCalendar();});
-    onRef('calendarNextBtn','click',()=>{global.calendarCursor=new Date(global.calendarCursor.getFullYear(),global.calendarCursor.getMonth()+1,1);global.renderCalendar();});
+    onRef('calendarPrevBtn','click',()=>{
+      const cursor=global.calendarCursor||calendarCursor||new Date();
+      const next=new Date(cursor.getFullYear(),cursor.getMonth()-1,1);
+      if(typeof global.calendarCursor!=='undefined') global.calendarCursor=next;
+      calendarCursor=next;
+      global.renderCalendar();
+    });
+    onRef('calendarNextBtn','click',()=>{
+      const cursor=global.calendarCursor||calendarCursor||new Date();
+      const next=new Date(cursor.getFullYear(),cursor.getMonth()+1,1);
+      if(typeof global.calendarCursor!=='undefined') global.calendarCursor=next;
+      calendarCursor=next;
+      global.renderCalendar();
+    });
   }
   global.registerCalendarBindings=registerCalendarBindings;
 })(typeof window!=='undefined'?window:globalThis);
