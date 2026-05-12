fb1f54d91644aada6947c1752dbb62ada1860e0f
Kina <gaireke@gmail.com>
2026-05-12 10:49:27 +0800

    Fix calendar cursor null/undefined handling in bindings

--- ui/bindings/calendar-bindings.js
+++ ui/bindings/calendar-bindings.js
@@ -1,20 +1,29 @@
 (function(global){
+  function resolveCalendarCursor(){
+    if(global.calendarCursor instanceof Date&&!Number.isNaN(global.calendarCursor.getTime())) return global.calendarCursor;
+    if(typeof calendarCursor!=='undefined'&&calendarCursor instanceof Date&&!Number.isNaN(calendarCursor.getTime())) return calendarCursor;
+    return new Date();
+  }
+
+  function syncCalendarCursor(next){
+    global.calendarCursor=next;
+    if(typeof calendarCursor!=='undefined') calendarCursor=next;
+  }
+
   function registerCalendarBindings(deps={}){
     const onRef=deps.on||global.on;
     const toggleCalendarView=deps.toggleCalendarView||global.toggleCalendarView;
     onRef('calendarBackBtn','click',()=>toggleCalendarView(false));
     onRef('calendarPrevBtn','click',()=>{
-      const cursor=global.calendarCursor||calendarCursor||new Date();
+      const cursor=resolveCalendarCursor();
       const next=new Date(cursor.getFullYear(),cursor.getMonth()-1,1);
-      if(typeof global.calendarCursor!=='undefined') global.calendarCursor=next;
-      calendarCursor=next;
+      syncCalendarCursor(next);
       global.renderCalendar();
     });
     onRef('calendarNextBtn','click',()=>{
-      const cursor=global.calendarCursor||calendarCursor||new Date();
+      const cursor=resolveCalendarCursor();
       const next=new Date(cursor.getFullYear(),cursor.getMonth()+1,1);
-      if(typeof global.calendarCursor!=='undefined') global.calendarCursor=next;
-      calendarCursor=next;
+      syncCalendarCursor(next);
       global.renderCalendar();
     });
   }
